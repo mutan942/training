fb1b1a55747171ffad7006b075cec53460c395a9
mutan942 <37330256+mutan942@users.noreply.github.com>
2022-03-25 11:59:42 +0800

    Update foot.php

--- application/views/template/foot.php
+++ application/views/template/foot.php
@@ -1,10 +1,10 @@
 </div>
 </div>
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"
-    integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
 <script src="<?=base_url("assets/js/bootstrap.bundle.min.js");?>" crossorigin="anonymous">
 </script>
-<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/js/select2.full.min.js"></script>
+
 <script type="text/javascript" src="https://cdn.datatables.net/v/bs5/dt-1.11.5/datatables.min.js"></script>
 <script>
 $(document).ready(function() {
